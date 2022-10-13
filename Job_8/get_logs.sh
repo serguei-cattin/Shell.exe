4affcd89f010d39f9716949037992a499b668840
Serguei Cattin <serguei.cattin@laplateforme.io>
2022-10-13 11:10:05 +0200

    changement du chemin d'acces du Job8 pour que cela refonctionne

--- Job_8/get_logs.sh
+++ Job_8/get_logs.sh
@@ -4,8 +4,8 @@
 DATE=$(date +%d-%m-%y-%H:%M)
 fichier=number_connection-$DATE
 last pikadmin1|wc -l >> $fichier
-tar -czvf /home/pikadmin1/Shell/Job_8/$fichier.tar.gz $fichier
+tar -czvf /home/pikadmin1/Shell.exe/Job_8/$fichier.tar.gz $fichier
 
 rm $fichier
-mv $fichier.tar.gz /home/pikadmin1/Shell/Job_8/Backup
+mv $fichier.tar.gz /home/pikadmin1/Shell.exe/Job_8/Backup
 
